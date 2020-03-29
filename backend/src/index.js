a2d4bf6d3a697716dc959d2edc84266052202af6
rogeriofox <rfp2005@gmail.com>
2020-03-29 02:35:55 -0300

    Conclusão da aula do segundo dia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+<<<<<<< HEAD
+=======
+const cors = require('cors');
+>>>>>>> Conclusão da aula do segundo dia
 const routes = require('./routes');
 
 const app = express();
 
+<<<<<<< HEAD
+=======
+app.use(cors());
+>>>>>>> Conclusão da aula do segundo dia
 app.use(express.json());
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,30 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+<<<<<<< HEAD
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+=======
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+>>>>>>> Conclusão da aula do segundo dia
 module.exports = routes;
